f561a29b8b4468eeeb58259f2da535094aa44d9b
shibil-thora <shibilallakkat123@gmail.com>
2024-06-23 07:22:02 +0530

    modified settings database default

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,7 +127,10 @@ DATABASES = {
     }} 
 
 if not os.environ.get('LOCAL') == 'True': 
-    DATABASES['default'] = dj_database_url.parse(os.environ.get('DB_URL'))
+    DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 
 # Password validation
