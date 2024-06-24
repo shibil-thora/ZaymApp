8822af849db4d171b0cbb411e0b8a05830b6a099
shibil-thora <shibilallakkat123@gmail.com>
2024-06-24 17:47:15 +0530

    asgi file updation

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator 
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings') 
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(), 
