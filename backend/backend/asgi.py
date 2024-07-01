45d9a88cdfe04082c0c1e7f4e349bd9f501ff936
shibil-thora <shibilallakkat123@gmail.com>
2024-07-01 10:28:57 +0530

    added load_dotenv() function in the asgi conf

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-import django
+import django 
+from dotenv import load_dotenv
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,7 +17,8 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings') 
-django.setup()
+django.setup() 
+load_dotenv()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(), 
